a75d7859d2d15d74c5e2c71b685fbaaa99f272cc
BaconnishRI <noelbacon08@gmail.com>
2026-07-13 20:27:18 -0400

    Screenshot Fix
    Issue with screenshot not working properly on release, update fixes it.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.baconnish.gobuddy"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0-dev"
+        versionCode = 2
+        versionName = "0.1.1-dev"
     }
 
     signingConfigs {
